17cec42e5dca88fe5af24fa4d248c3984c6f8107
nao0725 <aqua.725.times@gmail.com>
2021-07-30 05:16:23 +0000

    Create Categorys MC

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_045649) do
+ActiveRecord::Schema.define(version: 2021_07_30_051532) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2021_07_29_045649) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "rate"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "favorites", force: :cascade do |t|
